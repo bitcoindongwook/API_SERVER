c388b33865ec0ae4a30f0a9add4f55c6292f613b
dongwookseo <bitcoinseo@bluepan.net>
2016-11-16 17:15:29 +0900

    cmd로 변경

--- routes/getinfo.js
+++ routes/getinfo.js
@@ -4,18 +4,25 @@ var bitcoin = require('bitcoin');
 
 var client = new bitcoin.Client({
   host: 'localhost',
-  port: 18332,
-  user: 'dwdw',
-  pass: 'ehddnr0412'
+  port: 8332,
+  user: 'bitcoinrpc',
+  pass: 'GikPzS3yh5vB79gLmKWKF7cUyzEV8xXB1Nxr4g4nF3NB'
 });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
 	
-	client.getBalance('*', 6, function(err, balance, resHeaders) {
+	var batch = [];
+       batch.push({
+   	 	method: 'getinfo'
+
+  	});
+	client.cmd(batch, function(err, getinfo, resHeaders){
   		if (err) return console.log(err);
-  		res.json({"balance":balance});
+  		///console.log('getinfo:',getinfo.balance);
+  		res.json({"Balance":getinfo.balance});
+  		//console.log('sendtxid:', sendtxid);
 	});
 });
 
